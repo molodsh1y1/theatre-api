ec3354269756c6be31ec4a3830b02fe698a0752c
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2024-12-25 15:31:39 +0200

    Create custom user model and use email instead of username register user in admin panel

--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "theatre",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -126,6 +127,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.User"
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
