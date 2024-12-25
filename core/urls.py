40a1733d592849c4b58643d46b658d8daffa75ae
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2024-12-25 14:19:16 +0200

    Create models for theatre app and add empty files for project setup; install dependencies

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    path("api-theatre/", include("theatre.urls", namespace="theatre"))
+    path("api-theatre/", include("theatre.urls", namespace="theatre")),
+    path("api-accounts/", include("accounts.urls", namespace="accounts"))
 ]
--- theatre/models.py
+++ theatre/models.py
@@ -1,3 +1,113 @@
 from django.db import models
+from django.core.validators import (
+    MaxValueValidator,
+    MinValueValidator
+)
+from django.conf import settings
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["last_name", "first_name"]),
+            models.Index(fields=["first_name"], name="first_name_idx"),
+            models.Index(fields=["last_name"], name="last_name_idx")
+        ]
+
+
+class Play(models.Model):
+    title = models.CharField(max_length=63)
+    description = models.TextField(null=True, blank=True)
+    actors = models.ManyToManyField(Actor, related_name="plays")
+    genres = models.ManyToManyField(Genre, related_name="plays")
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ["title"]
+
+
+class TheatreHall(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+    rows = models.PositiveIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(100)]
+    )
+    seats_in_row = models.PositiveIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(100)]
+    )
+
+    def __str__(self) -> str:
+        return f"Theater: {self.rows} rows, {self.seats_in_row} seats per row"
+
+
+class Performance(models.Model):
+    play = models.ForeignKey(
+        Play,
+        on_delete=models.CASCADE,
+        related_name="performance"
+    )
+    theatre_hall = models.ForeignKey(
+        TheatreHall,
+        on_delete=models.CASCADE,
+        related_name="performance"
+    )
+    show_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return (
+            f"Performance of {self.play.title} at {self.theatre_hall.name}"
+            f" on {self.show_time.strftime('%Y-%m-%d %H:%M:%S')}"
+        )
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="reservations"
+    )
+
+    def __str__(self) -> str:
+        user_full_name = f"{self.user.first_name} {self.user.last_name}".strip()
+        formatted_created_at = self.created_at.strftime("%Y-%m-%d %H:%M:%S")
+        return f"Reserver {user_full_name} at {formatted_created_at}"
+
+
+class Ticket(models.Model):
+    row = models.PositiveIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(100)]
+    )
+    seat = models.PositiveIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(100)]
+    )
+    performance = models.ForeignKey(
+        Performance,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+    reservation = models.ForeignKey(
+        Reservation,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+
+    def __str__(self) -> str:
+        return f"Row: {self.row}" f"Seat: {self.seat}"
+
+    class Meta:
+        unique_together = ["row", "seat", "performance"]
