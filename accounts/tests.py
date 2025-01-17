e943cc7a8e002a91574ecc95efe4f0860905a267
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2025-01-17 21:58:39 +0200

    Write several test cases to accounts

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+USER_CREATE_URL = reverse("accounts:create")
+
+
+class CustomUserTest(APITestCase):
+    def test_create_user(self):
+        payload = {
+            "email": "email@test.com",
+            "password": "mypass1!s",
+        }
+        response = self.client.post(USER_CREATE_URL, payload)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_user_with_short_pass(self):
+        payload = {
+            "email": "email@test.com",
+            "password": "qq",
+        }
+        response = self.client.post(USER_CREATE_URL, payload)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_user_with_wrong_email(self):
+        payload = {
+            "email": "aaasssdddfff",
+            "password": "mypass1!s",
+        }
+        response = self.client.post(USER_CREATE_URL, payload)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
