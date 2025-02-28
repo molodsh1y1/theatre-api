6a3154479e20bd336fdb8f3662fe80b744b5265b
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2025-02-28 13:47:44 +0200

    Add secret key and debug in CI

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = get_env_variable("SECRET_KEY")
 
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = True
+DEBUG = get_env_variable("DEBUG") == "True"
 
 ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0", "localhost"]
 
